66869c677fc1d823e714a5f2325f84205713511f
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 15:03:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,34 @@ const codes = [
   "a"
 ];
 
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+let index = 0
+
 function init() {
   // your code here
-  document.body.addEventList
+  document.body.addEventListener("keydown", function (e) {
+
+    console.log(`Button pressed is ${e.key} and index is ${index}`)
+    
+    if (e.key === codes[index])
+      index++
+    else
+      index = 0
+
+    if (index === codes.length)
+    {
+      alert("You did it!")
+    }
+  })
 }
