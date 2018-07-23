defb84103b23533bd2d2a3274d606f9897710cd0
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 15:05:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,18 +11,7 @@ const codes = [
   "a"
 ];
 
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+
 let index = 0
 
 function init() {
